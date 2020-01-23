b7e0c568b9474e911caf468568bf0d3bfc5e2b35
cficht <chris.ficht@gmail.com>
2020-01-23 10:49:48 -0800

    cleaned and added comments to day2 additions

--- shopping-cart/shopping-cart.js
+++ shopping-cart/shopping-cart.js
@@ -6,11 +6,18 @@ import renderLineItem from './render-line-item.js';
 const shoppingTable = document.getElementById('shopping-table-body');
 const shoppingTableTotal = document.getElementById('shopping-table-total');
 
+// run through all items in cart
 for (let i = 0; i < selectedProducts.length; i++) {
+
+    // grab item from cart and find the corresponding product
     const selectedProduct = selectedProducts[i];
     const cartToInventory = findById(selectedProduct.id, flavors);
+
+    // create DOM for the cart item and append it to the table
     const renderCartItem = renderLineItem(cartToInventory, selectedProduct);
     shoppingTable.appendChild(renderCartItem);
+
+    // calculate order total and append it to the table
     const renderItemTotal = calcOrderItem(selectedProducts, flavors);
     shoppingTableTotal.textContent = `$${renderItemTotal.toFixed(2)}`;
 }
